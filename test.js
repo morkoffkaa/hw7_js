96c6909520020c0369d3eb8a9c399aa06500f821
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-07 23:27:35 +0200

    add project to git

---
+++ test.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Enter the first number');
+num1 = Number(num1);
+
+let num2 = prompt('Enter the second number');
+num2 = Number(num2);
+
+let num3 = prompt('Enter the third number');
+num3 = Number(num3);
+
+
+alert((num1 + num2 +num3) / 3);
