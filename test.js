1727e9f9ba929d2a6f366e99ec2600756f2c5a8b
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-08 19:22:59 +0200

    change js

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
 let numOrStr = prompt('input number or string', '');
 
 switch (true) {
-    case numOrStr === null:
+    case (numOrStr === null):
         console.log('ви скасували');
         break;
-    case numOrStr.trim() === '':
+    case (numOrStr.trim() === ''):
         console.log('Empty String');
         break;
-    case isNaN (+ numOrStr):
+    case (isNaN (+ numOrStr)):
         console.log('number is Ba_NaN')
         break;
     default:
         console.log('OK!')
         break;
 }
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+// console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+// console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+// console.log(' number is Ba_NaN')
+// } else {
+// console.log('OK!')
+// }
