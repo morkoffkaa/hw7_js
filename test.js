2b116dd03934217e25b63a700a30daa2d280c15f
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-08 15:34:43 +0200

    add project

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
-let num1 = prompt('Enter the first number');
-num1 = Number(num1);
+let numOrStr = prompt('input number or string', '');
 
-let num2 = prompt('Enter the second number');
-num2 = Number(num2);
-
-let num3 = prompt('Enter the third number');
-num3 = Number(num3);
-
-
-alert((num1 + num2 +num3) / 3);
+switch (true) {
+    case numOrStr === null:
+        console.log('Empty String');
+        break;
+    case numOrStr.trim() === '':
+        console.log('ви скасували');
+        break;
+    case isNaN (+ numOrStr):
+        console.log('number is Ba_NaN')
+        break;
+    default:
+        console.log('OK!')
+        break;
+}
